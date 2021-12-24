231f535e779cbed905204ea78da668c6080f391d
Erin <erin.braz@gmail.com>
2021-12-24 14:07:01 -0500

    adding more functionality

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 const recipeApp = {};
 
 recipeApp.init = () => {
-    recipeApp.getRecipes();
     recipeApp.getUserInput();
+    recipeApp.getRecipes(recipeApp.userChoices[0], recipeApp.userChoices[1],recipeApp.userChoices[2]);
 }
+
+recipeApp.userChoices = [];
 
 // Store url and key on gallery app object as a property
 recipeApp.url = 'https://api.spoonacular.com/recipes/findByIngredients'
 recipeApp.apiKey = '8f522d9d9210471691590e0132190021'
 
 // get some data! set up a fetch request to the unspash api
-recipeApp.getRecipes = () => {
+recipeApp.getRecipes = (ingredient1, ingredient2, ingredient3) => {
 
     // set up query string parameters via setting up a URL and URL search params object
     const url = new URL(recipeApp.url);
     url.search = new URLSearchParams({
-        ingredients: 'chicken,rice,onion',
-        number: 50,
+        ingredients: `${ingredient1},${ingredient2},${ingredient3}`,
+        number: 25,
         apiKey: recipeApp.apiKey
     });
 
@@ -28,6 +30,9 @@ recipeApp.getRecipes = () => {
         });
 }
 
+// recipeApp.recipes = [];
+
+
 recipeApp.getUserInput = () => {
     const dropdown1 = document.getElementById("ingredient1");
     const dropdown2 = document.getElementById("ingredient2");
@@ -35,14 +40,27 @@ recipeApp.getUserInput = () => {
 
     dropdown1.addEventListener("change", function () {
         console.log(dropdown1.value);
+
+        recipeApp.userChoices.splice(0, 1, dropdown1.value);
+
+        console.log(recipeApp.userChoices);
     });
 
     dropdown2.addEventListener("change", function () {
         console.log(dropdown2.value);
+
+        recipeApp.userChoices.splice(1, 1, dropdown2.value);
+
+        console.log(recipeApp.userChoices);
+
     });
 
     dropdown3.addEventListener("change", function () {
         console.log(dropdown3.value);
+
+        recipeApp.userChoices.splice(2, 1, dropdown3.value);
+
+        console.log(recipeApp.userChoices);
     });
 }
 
