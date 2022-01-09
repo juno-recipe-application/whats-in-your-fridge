760d5cf092b79c9fa80a82e402294013a9568f9d
Erin <erin.braz@gmail.com>
2022-01-09 12:58:02 -0500

    pushing changes for akeem

--- script.js
+++ script.js
@@ -7,11 +7,13 @@ recipeApp.init = () => {
 // make a global array to store the user's choices, which we will pass to our API call
 recipeApp.userChoices = [];
 
+// Make a global array that stores the returned recipes 
+recipeApp.recipes = [];
+
 // Store url and key on app object as a property
-// recipeApp.url = 'https://api.spoonacular.com/recipes/findByIngredients'
 recipeApp.url = 'https://api.spoonacular.com/recipes/complexSearch'
-// recipeApp.apiKey = '8f522d9d9210471691590e0132190021'
-recipeApp.apiKey = '6ca6794922364918a232cd5884e479a0'
+recipeApp.apiKey = '8f522d9d9210471691590e0132190021'
+// recipeApp.apiKey = '6ca6794922364918a232cd5884e479a0'
 
 // API call
 recipeApp.getRecipes = () => {
@@ -37,48 +39,29 @@ recipeApp.getRecipes = () => {
             jsonData.results.forEach((recipe) => {
                 recipeApp.recipes.push(recipe);
             })
+
             console.log(recipeApp.recipes);
             recipeApp.displayRecipes(recipeApp.recipes);
-
-            // console.log(jsonData.results);
-            // recipeApp.displayRecipes(jsonData.results);
         });
 }
 
-// Make a global array that stores the returned recipes 
-recipeApp.recipes = [];
 
 // capture user ingredient choices 
 recipeApp.getUserInput = () => {
-    // get our dropdown menus
-    // const dropdown1 = document.getElementById("ingredient1");
-    // const dropdown2 = document.getElementById("ingredient2");
-    // const dropdown3 = document.getElementById("ingredient3");
 
     // DOM to select form
     const userForm = document.getElementById('userForm')
-    // const submitIngredient = document.getElementById('submit');
 
     // create an event listener that will submit form on click
     userForm.addEventListener('submit', (e) => {
-
         e.preventDefault();
-        // recipeApp.userChoices = '';
+
         // create an array of ingredients from form to submit to api
-
-        //if using multiple select options in HTML
-
-        // for (var option of document.getElementById('ingredientMultiple').options) {
-        //     if (option.selected) {
-        //         recipeApp.userChoices.push(option.value);
-        //     }
-        // }
 
         //if using individual select boxes in HTML
         recipeApp.userChoices[0] = document.getElementById("ingredientOne").value;
         recipeApp.userChoices[1] = document.getElementById("ingredientTwo").value;
         recipeApp.userChoices[2] = document.getElementById("ingredientThree").value;
-
 
         // could use this as a prompt to display as html to double confirm choices 
         console.log("You have selected: " + recipeApp.userChoices);
@@ -91,10 +74,8 @@ recipeApp.getUserInput = () => {
 recipeApp.displayRecipes = (apiData) => {
     // get our result section
     const recipeSection = document.getElementsByClassName('results');
-    console.log(recipeSection);
 
-    //empty our results section
-    recipeSection.innerHTML = '';
+    // empty our results section somehow
 
     apiData.forEach((recipe) => {
         // create divs with class for styling
@@ -114,13 +95,11 @@ recipeApp.displayRecipes = (apiData) => {
         const infoButton = document.createElement('button');
         infoButton.setAttribute('id', 'modalButton');
         infoButton.innerText = 'See More';
-
-        // append button to div
-        divElement.appendChild(infoButton);
-
+        
         // append info to our div elements
         divElement.appendChild(image);
         divElement.appendChild(recipeHeading);
+        divElement.appendChild(infoButton);
 
         // append div to section
         recipeSection[0].appendChild(divElement);
@@ -128,6 +107,7 @@ recipeApp.displayRecipes = (apiData) => {
         // get div element for modal
         const modal = document.getElementById("moreInfoModal");
 
+        // event listener for modal button
         infoButton.addEventListener('click', function () {
             document.getElementById('moreInfoModal').style.visibility = 'visible';
 
@@ -136,7 +116,8 @@ recipeApp.displayRecipes = (apiData) => {
             const source = document.getElementById('sourceInfo');
             const summeryInfo = document.getElementById('dishInfo');
             const urlInfo = document.getElementById('webAddress');
-            const extendedIngredients = document.getElementById('extendedIngredients')
+            const extendedIngredients = document.getElementById('extendedIngredients');
+            const missingIngredients = document.getElementById('missingIngredients');
     
             // fill modal with info
             cuisineInfo.innerHTML = recipe.cuisines;
@@ -144,28 +125,26 @@ recipeApp.displayRecipes = (apiData) => {
             summeryInfo.innerHTML = recipe.summary;
             urlInfo.innerText = recipe.spoonacularSourceUrl;
 
-            // write a function here that loops through the extended ingredients array and displays each title ***and image*** 
+            // loop through extended ingredients array and display each item
             recipe.extendedIngredients.forEach((item) => {
                 const ingredient = document.createElement('li');
                 extendedIngredients.appendChild(ingredient);
                 ingredient.innerText = item.original;
             })
             
-            // write a function here that does the same thing for the missing ingredients
+            // same thing for the missing ingredients
+            recipe.missedIngredients.forEach((item) => {
+                const ingredient = document.createElement('li');
+                missingIngredients.appendChild(ingredient);
+                ingredient.innerText = item.original;
+            })
         });
 
         window.addEventListener('click', function (event) {
             if (event.target == moreInfoModal) {
                 document.getElementById('moreInfoModal').style.visibility = "hidden"
             }
-        });
-
-
-
-
-
-
-
+        })
     })
 }
 
